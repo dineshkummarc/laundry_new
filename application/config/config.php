23b6bfc221954cbada9f95a155794752fbe83fa4
Dinesh Kumar Choudhary <mapmydestination@gmail.com>
2020-09-24 04:20:08 +0530

    added

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/laundry';
+$config['base_url'] = 'http://local.rocket.co.in:8081/test1/laundry_new/';
 
 /*
 |--------------------------------------------------------------------------
